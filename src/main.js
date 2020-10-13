ea6a0495a42c125f60a68695033cc5cb6ad192be
小多多 <Dduo511@163.com>
2020-10-13 15:59:00 +0800

    项目初始化

--- src/main.js
+++ src/main.js
@@ -1,6 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import http from './utils/request'
+import 'element-ui/lib/theme-chalk/index.css'
+
+Vue.use(ElementUI)
+
+Vue.prototype.$http = http
 
 Vue.config.productionTip = false
 
